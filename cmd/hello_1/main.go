356ca68d3228621a04620be813b5160f1df8ffa2
rdv <RomanenkovDV@gmail.com>
2020-10-23 02:50:21 +0300

    Hello World

---
+++ cmd/hello_1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("HELLO 1")
+	fmt.Println("Hey, sup!")
+	fmt.Println("Hale! Bro")
+}
---
+++ cmd/hello_2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("HELLO 2")
+	fmt.Println("Hey, sup!")
+	fmt.Println("Hale! Bro")
+}
